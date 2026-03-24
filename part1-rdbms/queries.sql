9ec8a84d15cabdfc73c4f53a70b8771a79affa30
codecorner1983 <pallavikarnati@itformula1.com>
2026-03-24 09:17:53 +0530

    part 1 recommit

--- part1-rdbms/queries.sql
+++ part1-rdbms/queries.sql
@@ -12,6 +12,14 @@ JOIN products p ON oi.product_id = p.product_id
 WHERE c.city = 'Mumbai'
 GROUP BY c.customer_id, c.customer_name;
 
+o/p
+
+| customer_id | customer_name | total_order_value |
+| ----------- | ------------- | ----------------- |
+| C001        | Rohan Mehta   | 326390            |
+| C005        | Vikram Singh  | 854280            |
+
+
 
 -- =========================
 -- Q2: Find the top 3 products by total quantity sold
@@ -26,6 +34,13 @@ GROUP BY p.product_id, p.product_name
 ORDER BY total_quantity_sold DESC
 LIMIT 3;
 
+o/p: 
+
+prod_id	     product_name         total_quantity_sold 	
+	
+PR02	     Cotton Kurti	   184
+PR04	     Running Shoes	   114
+PR07	     Yoga Mat	           114
 
 -- =========================
 -- Q3: List all sales representatives and the number of unique customers they have handled
@@ -38,6 +53,14 @@ FROM sales_reps sr
 LEFT JOIN orders o ON sr.sales_rep_id = o.sales_rep_id
 GROUP BY sr.sales_rep_id, sr.sales_rep_name;
 
+o/p:
+
+	
+	
+sales_rep_id		sales_rep_name 				unique_customers
+SR01      		Deepak Joshi					8
+SR02     		 Ravi Kumar					8
+SR03      		Anita Desai					8
 
 -- =========================
 -- Q4: Find all orders where the total value exceeds 10,000, sorted by value descending
@@ -53,6 +76,36 @@ HAVING total_value > 10000
 ORDER BY total_value DESC;
 
 
+o/p:
+ORD1061     1980000
+ORD1131     1980000
+ORD1095     1485000
+ORD1125     1485000
+ORD1048     1485000
+ORD1144    1485000
+ORD1025    990000
+ORD1098    990000
+ORD1148    770000
+ORD1076    770000
+ORD1101    616000
+ORD1054    495000
+ORD1163    462000
+ORD1091    462000
+ORD1169    255000
+ORD1012    154000
+ORD1093    154000
+ORD1075    153000
+ORD1155    153000
+ORD1094    153000
+ORD1090    102000
+ORD1035    51000
+ORD1033    20000
+ORD1022   20000
+ORD1143   19200
+ORD1132   13750
+ORD1118   13750
+ORD1166   12000
+
 -- =========================
 -- Q5: Identify any products that have never been ordered
 -- =========================
@@ -62,3 +115,8 @@ SELECT
 FROM products p
 LEFT JOIN order_items oi ON p.product_id = oi.product_id
 WHERE oi.product_id IS NULL;
+
+| product_id | product_name |
+| ---------- | ------------ |
+| P010       | Smart Watch  |
+| P015       | Tablet       |
